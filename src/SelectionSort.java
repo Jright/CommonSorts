c117c8ee03db67ca7efe774ec59a434f91909cac
minchao.zhu <minchao.zhu@ushow.media>
2017-07-03 17:37:13 +0800

    Selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,41 @@
+/**
+ * Created by jeremy on 17年7月3日.
+ */
+public class SelectionSort {
+
+
+    /*
+     * 
+     * */
+
+    private static int[] array = {5,7,3,2,9,0,1};
+
+    public static void main(String[] args){
+        selectionSort(array);
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void selectionSort(int[] arr){
+
+        int i,j,min,temp;
+        int len = arr.length;
+        for(i = 0;i < len - 1;i++){
+            //First select every element to compare
+            min = i;
+            for(j = i + 1;j < len;j ++){
+                //then run a loop,search for the minimum value,record it.
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            //after the inner loop ends,swap the value of the element we located in the outer loop and the minimum
+            //value we found inside the inner loop.
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+}
